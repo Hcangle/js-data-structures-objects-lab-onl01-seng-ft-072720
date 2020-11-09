26f7ce8b4c4d1f8d55f8173a53f9c1b79bec4b9b
Hannah Angle <hannahcangle@gmail.com>
2020-11-09 04:21:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-const drive = {};
+const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
   retun Object.assign({}. driver, { [key]: value });
